1575b2eed3e697901ce247a2b13f6157169bfaff
Felix Saaro <felix@saaro.ch>
2021-09-25 21:00:11 +0200

    Fix search

--- pages/api/search/[query].js
+++ pages/api/search/[query].js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+const index = require('../../../data/detail/search_index.json');
 
 const handler = (req, res) => {
-    let file = fs.readFileSync('data/detail/search_index.json');
-    let index = JSON.parse(file);
     const query = req.query.query.toLowerCase();
 
-    const results = index.filter(
-        (i) => i.name?.toLowerCase().includes(query)|| i.substance?.toLowerCase().includes(query)|| i.atc?.toLowerCase().includes(query)
+    const results = index.filter((i) => 
+        i.name?.toLowerCase().includes(query) ||
+        i.substance?.toLowerCase().includes(query) ||
+        i.atc?.toLowerCase().includes(query)
     );
 
     res.status(200).json(results || []);
