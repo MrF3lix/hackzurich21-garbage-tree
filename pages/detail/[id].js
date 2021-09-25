7b2b02e5cc66580ed6c2c61914a44dfd23255bda
Felix Saaro <felix@saaro.ch>
2021-09-25 21:56:31 +0200

    Set caching header

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -137,6 +137,11 @@ export const getServerSideProps = async (context) => {
     const response = await fetch(`https://garbage-tree-api.azurewebsites.net/api/GetInformation?act=${context.query.id}`);
     const payload = await response.json();
 
+    context.res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=10, stale-while-revalidate=59'
+    );
+
     return {
         props: {
             data: { ...payload, loading: false },
