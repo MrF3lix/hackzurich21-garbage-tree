2e2778530cceaf63ebe5d88d30c0dfb988355375
Felix Saaro <felix@saaro.ch>
2021-09-25 12:28:35 +0200

    Add react-use, connect detail api

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,8 +1,22 @@
 import { useRouter } from 'next/router';
+import { useAsync } from 'react-use';
 
 const Detail = () => {
     const router = useRouter();
     const { id } = router.query;
-    return <h1>Detail: {id}</h1>;
+    const data = useAsync(async () => {
+        const response = await fetch(`/api/detail/${id}`);
+        return await response.json();
+    }, [id]);
+
+    return (
+        <>
+            {data.loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <h1>Detail: {data.value?.name}</h1>
+            )}
+        </>
+    );
 };
 export default Detail;
