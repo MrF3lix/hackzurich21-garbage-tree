70d5a0b1f07925796741f1445c0da9c8e1ddba70
Felix Saaro <felix@saaro.ch>
2021-09-25 18:52:28 +0200

    Update api to use azure function

--- pages/api/detail/[id].js
+++ pages/api/detail/[id].js
@@ -1,9 +1,7 @@
-const fs = require('fs');
-
-const handler = (req, res) => {
-    let file = fs.readFileSync(`data/detail/${req.query.id}.json`);
-    let detail = JSON.parse(file);
-    res.status(200).json(detail);
+const handler = async (req, res) => {
+    const response = await fetch(`https://garbage-tree-api.azurewebsites.net/api/GetInformation?act=${req.query.id}`);
+    const payload = await response.json();
+    res.status(200).json(payload);
 };
 
 export default handler;
